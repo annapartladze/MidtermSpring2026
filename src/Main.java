28412208247fc790f5f7bba5160c8ff01c9e02a7
Anna Partladze <ana.fartla@gmail.com>
2026-05-22 23:39:56 +0400

    Extract console interaction logic

--- src/Main.java
+++ src/Main.java
@@ -295,27 +295,6 @@ public class Main {
         return deck.remove(0);
     }
 
-    static String askColor() {
-        while (true) {
-            System.out.print("Call color R/Y/G/B: ");
-            String input = scanner.nextLine().trim().toUpperCase();
-            if (input.equals("R")) {
-                return "R";
-            }
-            if (input.equals("Y")) {
-                return "Y";
-            }
-            if (input.equals("G")) {
-                return "G";
-            }
-            if (input.equals("B")) {
-                return "B";
-            }
-            System.out.println("Bad color.");
-        }
-    }
-
-
 
 
     static void next() {
